5fcb1a67e4be75b09e6c2b49dadc720bd2d3ef01
GLong-code <70829262+GLong-code@users.noreply.github.com>
2021-11-08 14:51:01 +0700

    Add files via upload

---
+++ Dump20211108/foodstoredb_food.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: foodstoredb
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `food`
+--
+
+DROP TABLE IF EXISTS `food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `food` (
+  `foodID` int NOT NULL AUTO_INCREMENT,
+  `describeFood` varchar(255) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `importDay` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expiry` datetime NOT NULL,
+  `foodClass` int NOT NULL,
+  `atribute` double NOT NULL,
+  PRIMARY KEY (`foodID`),
+  KEY `foodClass` (`foodClass`),
+  CONSTRAINT `food_ibfk_1` FOREIGN KEY (`foodClass`) REFERENCES `foodClass` (`foodClass`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food`
+--
+
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (12,'Beef',4,'2021-11-05 17:10:29','2021-11-30 00:00:00',2,10),(13,'Chicken',10,'2021-11-05 17:10:29','2022-10-10 00:00:00',2,100),(14,'Fish Can',1,'2021-11-05 17:10:29','2021-11-20 00:00:00',1,5),(15,'Beef',4,'2021-11-05 17:10:55','2021-11-30 00:00:00',2,10),(16,'Chicken',10,'2021-11-05 17:10:55','2022-10-10 00:00:00',2,100),(17,'Fish Can',1,'2021-11-05 17:10:55','2021-11-20 00:00:00',1,5),(18,'Beef',4,'2021-11-05 17:11:51','2021-11-30 00:00:00',2,10),(19,'Chicken',10,'2021-11-05 17:11:52','2022-10-10 00:00:00',2,100),(20,'Fish Can',1,'2021-11-05 17:11:52','2021-11-20 00:00:00',1,5),(21,'Beef',4,'2021-11-05 17:22:00','2022-11-05 00:00:00',2,10),(22,'Fish Can',1,'2021-11-05 17:25:02','2021-10-20 00:00:00',1,5),(23,'Fish Can',1,'2021-11-05 20:25:58','2021-10-20 00:00:00',1,5);
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-08 14:45:11
